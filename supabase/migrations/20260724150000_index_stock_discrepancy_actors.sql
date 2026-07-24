054e2c0d7eac25cd3cbbf2b9310a4115896a96fe
Optima Idea <optmaidea@gmail.com>
2026-07-24 14:53:41 -0300

    chore(db): remove superseded actor index filename

--- supabase/migrations/20260724150000_index_stock_discrepancy_actors.sql
+++
@@ -1,7 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_stock_discrepancy_occurrences_created_by
-  ON public.stock_discrepancy_occurrences (created_by)
-  WHERE created_by IS NOT NULL;
-
-CREATE INDEX IF NOT EXISTS idx_stock_discrepancy_occurrences_resolved_by
-  ON public.stock_discrepancy_occurrences (resolved_by)
-  WHERE resolved_by IS NOT NULL;
