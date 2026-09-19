1472c036e10596ee891d83c63685956b56afb7df
Optima Idea <optmaidea@gmail.com>
2026-09-19 14:43:35 -0300

    security: restringe trigger de reserva delivery

---
+++ supabase/migrations/20260919144325_harden_delivery_reservation_trigger_grants.sql
@@ -0,0 +1,2 @@
+revoke all on function public.enforce_delivery_stock_reservation_lifetime() from public, anon, authenticated;
+grant execute on function public.enforce_delivery_stock_reservation_lifetime() to service_role;
